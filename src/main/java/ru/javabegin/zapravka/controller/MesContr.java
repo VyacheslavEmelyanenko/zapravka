0a099e86fb9c39c799b3b4b49939184a50d8989b
Slava <evaeyv@mail.ru>
2020-01-08 15:28:42 +0300

    change path

--- src/main/java/ru/javabegin/zapravka/controller/MesContr.java
+++ src/main/java/ru/javabegin/zapravka/controller/MesContr.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/1")
 public class MesContr {
     @GetMapping
     String home() {
