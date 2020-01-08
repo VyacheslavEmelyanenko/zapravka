f0befc7125586deafd863e8d39bc3952dc905bd3
Slava <evaeyv@mail.ru>
2020-01-08 16:11:53 +0300

    change world 1 2 3

--- src/main/java/ru/javabegin/zapravka/controller/MesContr.java
+++ src/main/java/ru/javabegin/zapravka/controller/MesContr.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MesContr {
     @GetMapping
     String home() {
-        return "Hello Slava!12";
+        return "Hello Slava!123";
     }
 }
